678566b1f133d8d30fe905d3c2948bd148439547
Curvezy <103641460@student.swin.edu.au>
2021-04-26 12:27:23 +1000

    challenge1

--- Week6/script.js
+++
@@ -1,6 +0,0 @@
-function NewItem() {
-    var li = document.createElement("li");
-    var Input = document.getElementById("UserInput");
-    var Text = document.createTextNode(Input);
-    document.getElementById("NewUl").appendChild(li);
-}
